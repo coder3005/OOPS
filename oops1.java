5d39b354917cebe815f1d466ec458b5a161d9d04
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-28 22:30:19 +0530

    Create oops1.java

---
+++ oops1.java
@@ -0,0 +1,44 @@
+package OOPS;
+
+public class OOPS {
+    public static void main(String args[]) {
+        Pen p1=new Pen(); //created a pen object called p1
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.setColor("Yellow");
+        System.out.println(p1.color);
+        p1.color="Red";
+        System.out.println(p1.color);
+
+        BankAccount myAcc=new BankAccount();
+        myAcc.username="AbhiGupta";
+        myAcc.password="abjal"; // error in this line
+        myAcc.setPassword("abcdefghi");
+    }
+}
+class BankAccount {
+    public String username;
+    private String password;
+    public void setPassword(String pwd) {
+        password=pwd;
+    }
+}
+class Pen {
+    String color;
+    int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    void setColor(String newColor) {
+        this.color=newColor;
+    }
+    
+    void setTip(int newTip) {
+        this.tip=newTip;
+    }
+}
+
