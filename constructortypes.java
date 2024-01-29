7cbfcf635b0a6902d8a3d2d5a3e22e439480f438
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-29 13:29:36 +0530

    Create constructortypes.java

---
+++ constructortypes.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+public class OOPS {
+    public static void main(String args[]) {
+        Student s1=new Student();
+        Student s2=new Student("Abhi");
+        Student s3=new Student(68);
+        //System.out.println(s1.name);
+
+    }
+}
+
+class Student {
+    String name;
+    int roll;
+
+    Student() { // non parameterized constructor
+        System.out.println("constructor is called..."); 
+    }
+    Student(String name) { // parameterized constructor
+        this.name=name;
+    }
+    Student(int roll) { // parameterized constructor
+        this.roll=roll;
+    }
+}
