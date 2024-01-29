5bbd67d123c9823af637f8e2cd8e131ec4f76cc1
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-29 18:58:08 +0530

    Create userdefinedpackage.java

---
+++ userdefinedpackage.java
@@ -0,0 +1,11 @@
+package OOPS; //including this
+
+import java.util.*;
+
+public class OOPS {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}
