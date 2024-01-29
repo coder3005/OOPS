a3053f384ab8a08abe36a896776b5bb4a26c9d4f
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-29 19:11:57 +0530

    Create abstractclass.java

---
+++ abstractclass.java
@@ -0,0 +1,43 @@
+package OOPS; //including this
+
+import java.util.*;
+
+public class OOPS {
+    public static void main(String[] args) {
+        Horse h=new Horse();
+        h.eat();
+        h.walk();
+        System.out.println(h.color);
+
+        Chicken c=new Chicken();
+        c.eat();
+        c.walk();
+        System.out.println(c.color);
+    }
+}
+abstract class Animal {
+    String color;
+    Animal() { //constructor
+        color="brown";
+    }
+    void eat() {
+        System.out.println("animal eats");
+    }
+    abstract void walk();
+}
+class Horse extends Animal {
+    void changeColor() {
+        color="dark brown"; //sabse pahele upar wala hi call hoga to brown hi pring hoga
+    }
+    void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
+class Chicken extends Animal {
+    void changeColor() {
+        color="yellow"; //sabse pahele upar wala hi call hoga to brown hi pring hoga
+    }
+    void walk() {
+        System.out.println("walks on 2 legs");
+    }
+}
