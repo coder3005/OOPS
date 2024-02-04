717da1a8c3e5c48155f628ed898a346810d97600
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 22:49:21 +0530

    Create method.java

---
+++ method.java
@@ -0,0 +1,13 @@
+/* Java Methods Program */
+class A 
+{
+    public static void main(String[] args) {
+        
+        A r=new A();
+        r.Disp();
+    }
+    void Disp()
+    {
+        System.out.print("Learn Coding");
+    }
+}
