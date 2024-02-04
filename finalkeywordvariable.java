7cc983bfd96ba667e28771464d523793878a2832
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 22:47:28 +0530

    Create finalkeywordvariable.java

---
+++ finalkeywordvariable.java
@@ -0,0 +1,12 @@
+/* final keyword */
+class Final 
+{
+    public static void main(String[] args) {
+
+        final int A=10;
+        System.out.println(A);
+
+     //   A=20; // not possible
+       // System.out.println(A);
+    }
+}
