15ac41f54e528698ba030364bcc085d17fbb69aa
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 22:39:39 +0530

    Create parameterizedconstructor.java

---
+++ parameterizedconstructor.java
@@ -0,0 +1,26 @@
+/* Parameterized Constructor */
+
+class T 
+{
+    int x,y;
+    T(int a, int b)//parameterized
+    {
+        x=a; y=b;
+    }
+    T(int a, String b)//parameterized
+    {
+        System.out.println(a+" "+b);
+    }
+    void show()
+    {
+        System.out.print(x+" "+y);
+    }
+}
+class R 
+{
+    public static void main(String[] args) {
+        T r=new T(100,200);
+        T ref=new T(10,"abhi");
+        r.show();
+    }
+}
