8dfcd30542adf9fc59bd0560120220876a0ef440
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 22:49:54 +0530

    Create method2.java

---
+++ method2.java
@@ -0,0 +1,22 @@
+/* Java Methods Program */
+class A 
+{
+    public static void Add()
+    {
+        int a=10,b=20;
+        System.out.print("Sum="+(a+b));
+    }
+    public static void main(String[] args) {
+        
+        A r=new A();
+        r.Disp();
+        A.Add();
+        A.Add();
+        A.Add();
+        A.Add();
+    }
+    void Disp()
+    {
+        System.out.print("Learn Coding\n");
+    }
+}
