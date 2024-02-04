c68d950caa0f7e4787d1873e3ca7717a3f8a03c5
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-04 22:44:57 +0530

    Create newkeyword1.java

---
+++ newkeyword1.java
@@ -0,0 +1,14 @@
+/* new keyword */
+class newKeyword
+{
+    int a=10;  // instance variable
+    newKeyword()
+    {
+        System.out.print(a);
+    }
+    public static void main(String[] args) {
+
+        newKeyword n=new newKeyword();
+        
+    }
+}
